06df65446c59269fdd860c77536c02069f5d4e96
Tim <herew26@gmail.com>
2023-01-08 02:36:59 +0300

    feat: add player movement component

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _speed = 12f;
+    
+    private bool _isGrounded;
+    
+    
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        _controller.Move(move * (_speed * Time.deltaTime));
+    }
+}
