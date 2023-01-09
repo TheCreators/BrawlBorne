4603e8eae934065c3a5fb463157d9cb82435fee8
Tim <herew26@gmail.com>
2023-01-09 04:44:46 +0300

    feat: implement new input system for moving and looking

--- Assets/Scripts/MouseRotation.cs
+++ Assets/Scripts/MouseRotation.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseRotation : MonoBehaviour
 {
     [SerializeField] private float _sensitivity = 1000f;
     [SerializeField] private Transform _playerBody;
     
-    private float _xRotation;
+    private Quaternion _rotation;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update()
+    public void OnLook(InputAction.CallbackContext context)
     {
-        float mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+        Vector2 mouseDelta = context.ReadValue<Vector2>();
         
-        _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        float mouseX = mouseDelta.x * _sensitivity * Time.deltaTime;
+        float mouseY = mouseDelta.y * _sensitivity * Time.deltaTime;
         
-        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _rotation.x -= mouseY;
+        _rotation.x = Mathf.Clamp(_rotation.x, -90f, 90f); // clamp the rotation to prevent the camera from flipping upside down
+        
+        transform.localRotation = Quaternion.Euler(_rotation.x, 0f, 0f);
         _playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController _controller;
     [SerializeField] private float _speed = 12f;
-    
-    private bool _isGrounded;
-    
+
+    private Vector2 _moveDirection;
     
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        
-        Vector3 move = transform.right * x + transform.forward * z;
-        
+        Move(_moveDirection);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+    }
+    
+    private void Move(Vector2 direction)
+    {
+        Vector3 move = transform.right * direction.x + transform.forward * direction.y;
         _controller.Move(move * (_speed * Time.deltaTime));
     }
 }
