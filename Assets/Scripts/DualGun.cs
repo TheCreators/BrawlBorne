5ec1cebc0a435cc126c375281b4e1a000ba48bc9
Tim <herew26@gmail.com>
2023-02-26 17:22:05 +0300

    fix: correct spawn point and reload time

--- Assets/Scripts/DualGun.cs
+++ Assets/Scripts/DualGun.cs
@@ -10,31 +10,56 @@ public class DualGun : MonoBehaviour
     [SerializeField] private Transform _camera;
     
     [Header("Settings")]
-    [SerializeField] private int _bulletsPerShot = 1;
-    [SerializeField] private float _timeBetweenBullets = 0.25f;
-    [SerializeField] private float _bulletsSpread = 0.5f;
-
+    [SerializeField] [Min(0)] private int _bulletsPerShot = 6;
+    [SerializeField] [Min(0)] private float _timeBetweenBullets = 0.25f;
+    [SerializeField] [Min(0)] private float _timeBetweenShots = 0.5f;
+    
+    [Header("Spawn Settings")]
+    [SerializeField] [Min(0)] private float _bulletsSpread = 0.5f;
+    [SerializeField] private float _bulletSpawnHeight = 0.5f;
+    
+    private bool _isShooting;
+    
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(Shoot());
+            Shoot();
         }
     }
     
-    private IEnumerator Shoot()
+    public void Shoot()
+    {
+        if (_isShooting)
+        {
+            return;
+        }
+        
+        _isShooting = true;
+        StartCoroutine(StartShooting());
+        StartCoroutine(StopShootingAfter(_timeBetweenShots));
+    }
+    
+    private IEnumerator StartShooting()
     {
         int positionShiftAmount = 1;
         for (int i = 0; i < _bulletsPerShot; i++)
         {
-            Vector3 spawnPosition = _camera.position + 
-                                    transform.forward + 
-                                    _bulletsSpread * positionShiftAmount * transform.right;
+            Vector3 spawnPosition = _camera.position + // Spawn position
+                                    _camera.forward + // Bullet direction
+                                    _camera.up * _bulletSpawnHeight + // Bullet spawn height
+                                    _bulletsSpread * positionShiftAmount * transform.right; // Spread (left or right)
             
             Instantiate(_bullet, spawnPosition, _camera.rotation);
             
             positionShiftAmount *= -1;
             yield return new WaitForSeconds(_timeBetweenBullets);
         }
+    }
+    
+    private IEnumerator StopShootingAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _isShooting = false;
     }
 }
