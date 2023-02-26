65940e76af1dd63ee4deb6eef1640ed06f2a7c78
Tim <herew26@gmail.com>
2023-02-26 20:34:31 +0300

    feat: add player shooting component

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [Header("Requirements")]
+    [SerializeField] private Gun _gun;
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _gun.Shoot();
+        }
+    }
+}
