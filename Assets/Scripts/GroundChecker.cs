452afa647d739ef8e4fc0d16bbfb840f9054bb6b
Tim <herew26@gmail.com>
2023-03-08 02:52:32 +0300

    feat: add ground checker back :)

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    [Header("Requirements")]
+    [SerializeField] private Transform _groundCheck;
+
+    [Header("Settings")]
+    [SerializeField] [Range(0, 5)] private float _checkRadius = 0.4f;
+    [SerializeField] private LayerMask _groundMask;
+
+    public bool IsGrounded => Physics.CheckBox(_groundCheck.position, Vector3.one * _checkRadius, Quaternion.identity, _groundMask);
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = IsGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireCube(_groundCheck.position, Vector3.one * _checkRadius);
+    }
+}
