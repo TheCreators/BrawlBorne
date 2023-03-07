78568ebe1f0a19706902ed6d83ae063afe397757
Tim <herew26@gmail.com>
2023-03-08 02:54:23 +0300

    feat: add is moving property

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Requirements")]
-    [SerializeField] private CharacterController _controller;
+    [Header("Requirements")] [SerializeField]
+    private CharacterController _controller;
 
-    [Header("Settings")]
-    [SerializeField] [Range(0, 50)] private float _normalSpeed = 12f;
+    [Header("Settings")] [SerializeField] [Range(0, 50)]
+    private float _normalSpeed = 12f;
+
     [SerializeField] [Range(0, 50)] private float _shiftSpeed = 6f;
     [SerializeField] [Range(0, 0.5f)] private float _moveDirectionSmoothingSpeed = 0.1f;
     [SerializeField] [Range(0, 0.5f)] private float _speedSmoothingSpeed = 0.1f;
@@ -15,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _inputMoveDirection;
     private Vector2 _smoothMoveDirection;
     private Vector2 _smoothMoveVelocity;
-    
+
     private float _speed;
     private float _smoothSpeed;
     private float _smoothSpeedVelocity;
-    
+
 
     private void Awake()
     {
@@ -35,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector2 NormalizedSmoothMoveDirectionWithSmoothSpeed => _smoothMoveDirection * _smoothSpeed / _normalSpeed;
 
+    public bool IsMoving => _inputMoveDirection != Vector2.zero;
+
     public void OnMove(InputAction.CallbackContext context)
     {
         _inputMoveDirection = context.ReadValue<Vector2>();
@@ -44,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
     {
         _speed = context.performed ? _shiftSpeed : _normalSpeed;
     }
-    
+
     private void SmoothValues()
     {
         _smoothMoveDirection = Vector2.SmoothDamp(_smoothMoveDirection, _inputMoveDirection, ref _smoothMoveVelocity, _moveDirectionSmoothingSpeed);
@@ -54,6 +57,6 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         var move = transform.right * _smoothMoveDirection.x + transform.forward * _smoothMoveDirection.y;
-        _controller.Move(move * _smoothSpeed * Time.deltaTime);
+        _controller.Move(move * (_smoothSpeed * Time.deltaTime));
     }
 }
