6b237cd5af6a03caa6315949fd81fc5203de000b
TimNekk <herew26@gmail.com>
2023-05-03 19:59:47 +0300

    feat: add heroes pool

---
+++ Assets/Scripts/Misc/HeroesPool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Misc
+{
+    public class HeroesPool : MonoSingleton<HeroesPool>
+    {
+        [Header("Requirements")]
+        [SerializeField] private GameObject _playerPrefab;
+        [SerializeField] private GameObject _botPrefab;
+        
+        [Header("Settings")]
+        [SerializeField] [Min(0)] private int _botsCount = 2;
+
+        public List<GameObject> Heroes { get; private set; } = new ();
+        
+        private void Start()
+        {
+            Heroes.Add(Instantiate(_playerPrefab));
+            
+            for (var i = 0; i < _botsCount; i++)
+            {
+                Heroes.Add(Instantiate(_botPrefab));
+            }
+            
+            Debug.Log($"Heroes count: {GetHashCode()}");
+        }
+        
+        private void OnDestroy()
+        {
+            foreach (var hero in Heroes)
+            {
+                Destroy(hero);
+            }
+        }
+    }
+}
