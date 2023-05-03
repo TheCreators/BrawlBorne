4752490eea2592bdb4d0f953eebbea8de8980961
TimNekk <herew26@gmail.com>
2023-05-03 20:00:17 +0300

    feat: add debug gizmos for bots

---
+++ Assets/Scripts/Bot/BotGizmos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bot
+{
+    public class BotGizmos : MonoBehaviour
+    {
+        [SerializeField] private BotController _botController;
+
+        private void Update()
+        {
+            switch (_botController.CurrentState)
+            {
+                case BotState.Wandering:
+                    Debug.DrawLine(transform.position, _botController.Agent.destination, Color.green);
+                    break;
+
+                case BotState.Chasing:
+                    Debug.DrawLine(transform.position, _botController.Agent.destination, Color.yellow);
+                    break;
+
+                case BotState.Attacking:
+                    if (_botController.Target == null) break;
+                    Debug.DrawLine(transform.position, _botController.Target.transform.position, Color.red);
+                    break;
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, _botController.RandomPointMaxRadius);
+            Gizmos.DrawWireSphere(transform.position, _botController.RandomPointMinRadius);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _botController.PlayerDetectionRadius);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _botController.PlayerAttackRadius);
+        }
+    }
+}
