6d277f3352bf04f8cedcdd2a4656ee188baee663
TimNekk <herew26@gmail.com>
2023-05-03 19:59:34 +0300

    feat: add mono singleton abstract class

---
+++ Assets/Scripts/Misc/MonoSingleton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+
+namespace Misc
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogError(typeof(T) + " is missing.");
+                }
+
+                return _instance;
+            }
+        }
+
+
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                Debug.LogWarning("Second instance of " + typeof(T) + " created. Automatic self-destruct triggered.");
+                Destroy(gameObject);
+            }
+
+            _instance = this as T;
+
+            Init();
+        }
+
+
+        private void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
+
+
+        protected virtual void Init()
+        {
+        }
+    }
+}
