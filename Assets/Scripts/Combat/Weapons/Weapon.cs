6a17eafd481f2a8f8d2fb61963e97338e821addd
Tim <herew26@gmail.com>
2023-05-08 20:51:15 +0300

    feat: add abstract weapon

---
+++ Assets/Scripts/Combat/Weapons/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Combat.Weapons
+{
+    public abstract class Weapon : MonoBehaviour
+    {
+        protected bool CanBeUsed = true;
+        
+        public void TryUse() {
+            if (CanBeUsed is false)
+            {
+                return;
+            }
+
+            Use();
+        }
+        
+        protected abstract void Use();
+    }
+}
