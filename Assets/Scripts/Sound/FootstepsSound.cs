6c407f783d9334d0c9019a563d215ea6e7422fcc
Tim <herew26@gmail.com>
2023-05-08 22:50:20 +0300

    ref: split into smaller chunks

--- Assets/Scripts/Sound/FootstepsSound.cs
+++ Assets/Scripts/Sound/FootstepsSound.cs
@@ -1,6 +1,4 @@
 using Misc;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sound
@@ -9,77 +7,97 @@ namespace Sound
     [RequireComponent(typeof(GroundChecker))]
     public class FootstepsSound : MonoBehaviour
     {
-        [Header("Requirements")]
-        [SerializeField] private AudioClip _footstepsSound;
+        [Header("Requirements")] [SerializeField]
+        private AudioClip _footstepsSound;
+
         [SerializeField] private AudioClip _jumpSound;
         [SerializeField] private AudioClip _landingSound;
         [SerializeField] private AudioSource _audioSource;
-
         private Rigidbody _character;
         private GroundChecker _groundChecker;
+        private bool _wasGrounded;
 
-        private bool wasGrounded = false;
-
-        void Start()
+        private void Start()
         {
             _audioSource = GetComponent<AudioSource>();
             _character = GetComponent<Rigidbody>();
             _groundChecker = GetComponent<GroundChecker>();
         }
 
-        void FixedUpdate()
+        private void Update()
         {
+            bool isGrounded = _groundChecker.IsGrounded;
+            bool isMoving = _character.velocity.sqrMagnitude > 0f;
+            bool isPlaying = _audioSource.isPlaying;
 
-            if (_groundChecker.IsGrounded)
+            if (isGrounded)
             {
-                if (_character.velocity.sqrMagnitude > 0f)          // Character is moving. sqrMagnitude is in less need for resources.
-                {
-                    if (!_audioSource.isPlaying)
-                    {
-                        _audioSource.clip = _footstepsSound;
-                        _audioSource.Play();
-                    }
-                }
-                else
-                {
-                    if (_audioSource.clip != _jumpSound && _audioSource.clip != _landingSound)
-                    {
-                        if (_audioSource.isPlaying)
-                        {
-                            _audioSource.Stop();
-                        }
-                    }
-                }
-
-                if (_groundChecker.IsGrounded != wasGrounded)
-                {
-                    _audioSource.Stop();
-                    _audioSource.clip = _landingSound;
-                    _audioSource.Play();
-
-                }
-
-                wasGrounded = true;
+                HandleGroundedState(isMoving, isPlaying);
             }
             else
             {
-                if (_audioSource.clip != _jumpSound && _audioSource.clip != _landingSound)
-                {
-                    if (_audioSource.isPlaying)
-                    {
-                        _audioSource.Stop();
-                    }
-                }
-
-                if (_groundChecker.IsGrounded != wasGrounded)
-                {
-                    _audioSource.Stop();
-                    _audioSource.clip = _jumpSound;
-                    _audioSource.Play();
-                }
-
-                wasGrounded = false;
+                HandleAirborneState(isPlaying);
             }
+        }
+
+        private void HandleGroundedState(bool isMoving, bool isPlaying)
+        {
+            if (isMoving)
+            {
+                PlayFootstepsSound(isPlaying);
+            }
+            else
+            {
+                StopNonJumpOrLandingSounds(isPlaying);
+            }
+
+            if (_groundChecker.IsGrounded != _wasGrounded)
+            {
+                PlayLandingSound();
+            }
+
+            _wasGrounded = true;
+        }
+
+        private void HandleAirborneState(bool isPlaying)
+        {
+            StopNonJumpOrLandingSounds(isPlaying);
+
+            if (_groundChecker.IsGrounded != _wasGrounded)
+            {
+                PlayJumpSound();
+            }
+
+            _wasGrounded = false;
+        }
+
+        private void PlayFootstepsSound(bool isPlaying)
+        {
+            if (isPlaying) return;
+            _audioSource.clip = _footstepsSound;
+            _audioSource.Play();
+        }
+
+        private void StopNonJumpOrLandingSounds(bool isPlaying)
+        {
+            if (_audioSource.clip != _jumpSound && _audioSource.clip != _landingSound && isPlaying)
+            {
+                _audioSource.Stop();
+            }
+        }
+
+        private void PlayLandingSound()
+        {
+            _audioSource.Stop();
+            _audioSource.clip = _landingSound;
+            _audioSource.Play();
+        }
+
+        private void PlayJumpSound()
+        {
+            _audioSource.Stop();
+            _audioSource.clip = _jumpSound;
+            _audioSource.Play();
         }
     }
 }
