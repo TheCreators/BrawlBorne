23fd2a6018f0cb0b1380ef25e7c7ff6be54941dc
Tim <herew26@gmail.com>
2023-05-08 20:48:37 +0300

    feat: add health system

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Combat
+{
+    public class Health : MonoBehaviour, IDamageable
+    {
+        [Header("Settings")]
+        [SerializeField, Min(0)] private float _healthPoints = 100f;
+
+        public void TakeDamage(float damage)
+        {
+            _healthPoints -= damage;
+
+            if (_healthPoints <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
