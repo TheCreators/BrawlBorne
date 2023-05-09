c951801b9c89989192f61796955e50c54c40f8ae
Yanulio <yagshulepa@edu.hse.ru>
2023-05-09 15:21:42 +0300

    feat: add health restoration logic

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Combat
 {
     public class Health : MonoBehaviour, IDamageable
     {
         [Header("Settings")]
+        [SerializeField, Min(0)] private float _maxHealthPoints = 100f;
         [SerializeField, Min(0)] private float _healthPoints = 100f;
+
+        private void Start()
+        {
+            _healthPoints = _maxHealthPoints;
+        }
 
         public void TakeDamage(float damage)
         {
@@ -14,6 +21,19 @@ namespace Combat
             if (_healthPoints <= 0)
             {
                 Destroy(gameObject);
+            }
+        }
+
+        public void Heal(float healedPointsPercent)
+        {
+            float healedValue = _maxHealthPoints * healedPointsPercent / 100;
+            if (_healthPoints + healedValue < _maxHealthPoints)
+            {
+                _healthPoints += healedValue;
+            }
+            else
+            {
+                _healthPoints = _maxHealthPoints;
             }
         }
     }
