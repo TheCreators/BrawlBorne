446d449a048439423cf2d43b2d3dd41cb090b024
Yanulio <yagshulepa@edu.hse.ru>
2023-05-09 15:27:18 +0300

    feat: add bats ultimate logic

---
+++ Assets/Scripts/Ultimates/BatsUltimate.cs
@@ -0,0 +1,20 @@
+using Combat.Weapons;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Ultimates
+{
+    public class BatsUltimate : MonoBehaviour
+    {
+        [Header("Requirements")]
+        [SerializeField] private Weapon _weapon;
+
+        public void OnUltimate(InputAction.CallbackContext context)
+        {
+            if (context.performed is true)
+            {
+                _weapon.TryUse();
+            }
+        }
+    }
+}
