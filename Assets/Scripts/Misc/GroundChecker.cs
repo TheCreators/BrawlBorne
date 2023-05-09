efb2c4504143cd22cdffdf761a1daf06301a045d
Tim <herew26@gmail.com>
2023-05-10 02:14:26 +0300

    feat: add onLand and onAirborne events

--- Assets/Scripts/Misc/GroundChecker.cs
+++ Assets/Scripts/Misc/GroundChecker.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Misc
 {
@@ -10,12 +11,27 @@ namespace Misc
         [Header("Settings")]
         [SerializeField, Range(0, 5)] private float _checkRadius = 0.4f;
         [SerializeField] private LayerMask _groundMask;
+        [SerializeField] private UnityEvent _onLand;
+        [SerializeField] private UnityEvent _onAirborne;
 
         public bool IsGrounded { get; private set; }
+        private bool _wasGrounded = false;
 
         private void Update()
         {
             IsGrounded = Physics.CheckBox(_groundCheck.position, Vector3.one * _checkRadius, Quaternion.identity, _groundMask);
+
+            if (IsGrounded && !_wasGrounded)
+            {
+                _onLand.Invoke();
+            }
+
+            if (!IsGrounded && _wasGrounded)
+            {
+                _onAirborne.Invoke();
+            }
+
+            _wasGrounded = IsGrounded;
         }
 
         private void OnDrawGizmosSelected()
