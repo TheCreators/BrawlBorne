fb007753e9b532edac1ba470346b034d0ca896b0
VladKhmel <vladkhmel22@gmail.com>
2023-05-10 17:16:35 +0300

    feat: added NavMesh baking after map creation

--- Assets/Scripts/Battlefield/BattleFieldBuilder.cs
+++ Assets/Scripts/Battlefield/BattleFieldBuilder.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BattleFieldBuilder : MonoBehaviour
 {
@@ -9,25 +11,31 @@ public class BattleFieldBuilder : MonoBehaviour
     public GameObject bottle;
     public GameObject obstacle;
     public GameObject ground;
-    
+
     private int fieldLength = 30;
     private int fieldWidth = 30;
-
+    private List<NavMeshSurface> navMeshSurfaces= new List <NavMeshSurface>();
     void Start()
     {
         Build();
+        BakeNavMesh();
     }
 
     void Build()
     {
+        GameObject fiel = Instantiate(ground, new Vector3(0, 0, 0), Quaternion.identity);
+        navMeshSurfaces.Add(fiel.GetComponentInChildren<NavMeshSurface>());
         float wallSize = 2;
 
-        BattleField map = new BattleFieldGenerator(new BattleField(fieldLength, fieldWidth)).GenerateExternalWalls().GenerateWalls()
+        BattleField map = new BattleFieldGenerator(new BattleField(fieldLength, fieldWidth)).GenerateExternalWalls()
+            .GenerateWalls()
             .DeleteSingleWalls().AddBushes().AddPots().BuildMap();
-        
+
         float xCorrection = -fieldLength * wallSize;
         float zCorrection = -fieldWidth * wallSize;
-        Instantiate(ground, new Vector3(0, 0, 0),Quaternion.identity);
+
+
+        GameObject createdObject;
         for (int i = 0; i < map.Cols; i++)
         {
             for (int j = 0; j < map.Rows; j++)
@@ -35,15 +43,20 @@ public class BattleFieldBuilder : MonoBehaviour
                 switch (map[i, j])
                 {
                     case 1:
-                        Instantiate(wall, new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
+                        createdObject = Instantiate(wall,
+                            new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
                             Quaternion.identity);
+                        navMeshSurfaces.Add((createdObject.GetComponentInChildren<NavMeshSurface>()));
                         break;
                     case 2:
-                        Instantiate(wall, new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
+                        createdObject = Instantiate(wall,
+                            new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
                             Quaternion.identity);
+                        navMeshSurfaces.Add((createdObject.GetComponentInChildren<NavMeshSurface>()));
                         break;
                     case 3:
-                        Instantiate(grass, new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
+                        Instantiate(grass,
+                            new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
                             Quaternion.identity);
                         break;
                     case 5:
@@ -51,11 +64,21 @@ public class BattleFieldBuilder : MonoBehaviour
                             Quaternion.identity);
                         break;
                     case 6:
-                        Instantiate(obstacle, new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
+                        createdObject = Instantiate(obstacle,
+                            new Vector3(xCorrection + i * wallSize, 0, zCorrection + j * wallSize),
                             Quaternion.identity);
+                        navMeshSurfaces.Add((createdObject.GetComponentInChildren<NavMeshSurface>()));
                         break;
                 }
             }
+        }
+    }
+
+    void BakeNavMesh()
+    {
+        for (int i = 0; i < navMeshSurfaces.Count; i++)
+        {
+            navMeshSurfaces[i].BuildNavMesh();
         }
     }
 
