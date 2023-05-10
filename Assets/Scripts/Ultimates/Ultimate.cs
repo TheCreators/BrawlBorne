3ae23061753793310870d5e51283e24775fb0154
Yanulio <yagshulepa@edu.hse.ru>
2023-05-10 15:38:24 +0300

    feat: add cooldown to ultimate base class

--- Assets/Scripts/Ultimates/Ultimate.cs
+++ Assets/Scripts/Ultimates/Ultimate.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ultimates
 {
     public abstract class Ultimate : MonoBehaviour
     {
+        [SerializeField] protected bool _canBeUsed = false;
+        [SerializeField, Min(0)] protected float _cooldown = 10f;
         public abstract void Use();
+
+        protected void SetCanBeUsedToTrue()
+        {
+            _canBeUsed = true;
+        }
     }
 }
