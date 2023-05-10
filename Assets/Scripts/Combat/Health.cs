1660cb211e33b776083f9125670e240a1d303c91
Tim <herew26@gmail.com>
2023-05-10 21:33:24 +0300

    fix: remove GetHealth

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -10,18 +10,14 @@ namespace Combat
         [SerializeField, Min(0)] private float _maxHealthPoints = 100f;
         [SerializeField, Min(0)] private float _healthPoints = 100f;
 
-        public float GetHealth { get; private set; }
-
         private void Start()
         {
             _healthPoints = _maxHealthPoints;
-            GetHealth = _healthPoints;
         }
 
         public void TakeDamage(float damage)
         {
             _healthPoints -= damage;
-            GetHealth = _healthPoints;
 
             if (_healthPoints <= 0)
             {
@@ -41,7 +37,6 @@ namespace Combat
             {
                 _healthPoints = _maxHealthPoints;
             }
-            GetHealth = _healthPoints;
         }
     }
 }
