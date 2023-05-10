4ce6b2e552019d9defdf6df7fae51f6549d7e745
Yanulio <yagshulepa@edu.hse.ru>
2023-05-10 14:56:28 +0300

    fix: fix a bug creating objects after the scene is unloaded

--- Assets/Scripts/Environment/Crate.cs
+++ Assets/Scripts/Environment/Crate.cs
@@ -8,6 +8,7 @@ namespace Environment
         [SerializeField] private GameObject _boost;
         private void OnDestroy()
         {
+            if (gameObject.scene.isLoaded is false) return;
             Instantiate(_boost, transform.position, transform.rotation);
         }
     }
