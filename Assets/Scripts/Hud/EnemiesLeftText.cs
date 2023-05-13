65ca1576ef0e3983fc375a42725bf56bb83b564e
Tim <herew26@gmail.com>
2023-05-13 13:56:21 +0300

    feat: add hud scripts

---
+++ Assets/Scripts/Hud/EnemiesLeftText.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Hud
+{
+    public class EnemiesLeftText : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+
+        public void SetEnemiesLeft(Component component, object data)
+        {
+            int enemiesLeft = (int) data;
+            _text.text = $"{enemiesLeft - 1}";
+        }
+    }
+}
