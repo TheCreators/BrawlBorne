cef20ede8902aed8903fdf91702d46543a0ba5ea
jaklbela <KsZh11@mail.ru>
2023-05-16 16:27:15 +0300

    Added beautiful textures and explosions

--- Assets/Scripts/Combat/Projectiles/Bomb.cs
+++ Assets/Scripts/Combat/Projectiles/Bomb.cs
@@ -5,6 +5,7 @@ namespace Combat.Projectiles
     public class Bomb : RigidbodyProjectile
     {
         [SerializeField] private LayerMask _hitLayers;
+        [SerializeField] private GameObject _explosion;
         [SerializeField, Min(0)] private float _timeToExplode = 3f;
         [SerializeField, Min(0)] private float _damage = 20f;
         [SerializeField, Min(0)] private float _explosionRadius = 5f;
@@ -16,6 +17,7 @@ namespace Combat.Projectiles
 
         private void Explode()
         {
+            Instantiate(_explosion, transform.position, _explosion.transform.rotation);
             var colliders = new Collider[20];
             int count = Physics.OverlapSphereNonAlloc(transform.position, _explosionRadius, colliders, _hitLayers);
 
