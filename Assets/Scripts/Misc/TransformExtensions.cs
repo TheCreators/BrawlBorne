1cce927383aa36317377f5ee6e9d7a3093e3f6bc
Tim <herew26@gmail.com>
2023-05-16 23:29:33 +0300

    feat: add Component and Transform extensions

---
+++ Assets/Scripts/Misc/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public static class TransformExtensions
+    {
+        public static string GetHierarchyPath(this Transform transform)
+        {
+            string path = transform.name;
+            while (transform.parent != null)
+            {
+                transform = transform.parent;
+                path = transform.name + "/" + path;
+            }
+            return path;
+        }
+    }
+}
