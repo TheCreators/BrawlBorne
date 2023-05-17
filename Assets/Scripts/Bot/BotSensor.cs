ee93b7be526dd36921461442e71e818f1aa97098
Tim <herew26@gmail.com>
2023-05-17 14:00:06 +0300

    feat: add crate and boost detection to bot sensor

--- Assets/Scripts/Bot/BotSensor.cs
+++ Assets/Scripts/Bot/BotSensor.cs
@@ -1,4 +1,6 @@
-﻿using Combat;
+﻿using System.Collections.Generic;
+using Combat;
+using Environment;
 using JetBrains.Annotations;
 using Misc;
 using UnityEngine;
@@ -7,49 +9,69 @@ namespace Bot
 {
     public class BotSensor : MonoBehaviour
     {
-        [SerializeField] private float _playerDetectionRadius = 7f;
-        [SerializeField] private float _playerAttackRadius = 5f;
+        [SerializeField] private float _playerDetectionRadius = 7f; // TODO: rename to _heroDetectionRange
+        [SerializeField] private Color _playerDetectionColor = Color.yellow;
+        
+        [SerializeField] private float _crateDetectionRadius = 10f;
+        [SerializeField] private Color _crateDetectionColor = Color.magenta;
+        
+        [SerializeField] private float _crateAttackRadius = 5f;
+        [SerializeField] private Color _crateAttackColor = Color.red;
+        
+        [SerializeField] private float _boostDetectionRadius = 10f;
+        [SerializeField] private Color _boostDetectionColor = Color.cyan;
+        
+        [SerializeField] private float _playerAttackRadius = 5f; // TODO: rename to _attackRange
+        [SerializeField] private Color _playerAttackColor = Color.red;
 
-        public bool IsAnyHeroInDetectionRange => IsAnyHeroInRange(_playerDetectionRadius);
-        public bool IsAnyHeroInAttackRange => IsAnyHeroInRange(_playerAttackRadius);
-        [CanBeNull] public Hero ClosestHeroInDetectionRange => GetClosestHeroInRange(_playerDetectionRadius);
-        [CanBeNull] public Hero ClosestHeroInAttackRange => GetClosestHeroInRange(_playerAttackRadius);
-
-        private bool IsAnyHeroInRange(float range)
+        public bool IsAnyHeroInDetectionRange => IsAnyObjectInRange(_playerDetectionRadius, ObjectsPool.Instance.Heroes);
+        [CanBeNull] public Hero ClosestHeroInDetectionRange => GetClosestObjectInRange(_playerDetectionRadius, ObjectsPool.Instance.Heroes);
+        
+        public bool IsAnyHeroInAttackRange => IsAnyObjectInRange(_playerAttackRadius, ObjectsPool.Instance.Heroes);
+        [CanBeNull] public Hero ClosestHeroInAttackRange => GetClosestObjectInRange(_playerAttackRadius, ObjectsPool.Instance.Heroes);
+        
+        public bool IsAnyCrateInDetectionRange => IsAnyObjectInRange(_crateDetectionRadius, ObjectsPool.Instance.Crates);
+        [CanBeNull] public Crate ClosestCrateInDetectionRange => GetClosestObjectInRange(_crateDetectionRadius, ObjectsPool.Instance.Crates);
+        
+        public bool IsAnyCrateInAttackRange => IsAnyObjectInRange(_crateAttackRadius, ObjectsPool.Instance.Crates);
+        [CanBeNull] public Crate ClosestCrateInAttackRange => GetClosestObjectInRange(_crateAttackRadius, ObjectsPool.Instance.Crates);
+        
+        public bool IsAnyBoostInDetectionRange => IsAnyObjectInRange(_boostDetectionRadius, ObjectsPool.Instance.Boosts);
+        [CanBeNull] public Boost ClosestBoostInDetectionRange => GetClosestObjectInRange(_boostDetectionRadius, ObjectsPool.Instance.Boosts);
+        
+        private bool IsAnyObjectInRange(float range, IEnumerable<Component> objects)
         {
-            foreach (Hero hero in HeroesPool.Instance.Heroes)
+            foreach (Component obj in objects)
             {
-                if (hero.gameObject == transform.gameObject) continue;
+                if (obj == null) continue;
+                
+                var distance = Vector3.Distance(transform.position, obj.transform.position);
 
-                var distance = Vector3.Distance(transform.position, hero.ShootAt);
-
-                if (distance > range) continue;
-
-                return true;
+                if (distance <= range) return true;
             }
 
             return false;
         }
 
         [CanBeNull]
-        private Hero GetClosestHeroInRange(float range)
+        private T GetClosestObjectInRange<T>(float range, IEnumerable<T> objects) where T : Component
         {
-            Hero closestHero = null;
+            T closestObject = null;
             var closestDistance = Mathf.Infinity;
 
-            foreach (Hero hero in HeroesPool.Instance.Heroes)
+            foreach (T obj in objects)
             {
-                if (hero.gameObject == transform.gameObject) continue;
+                if (obj == null) continue;
 
-                var distance = Vector3.Distance(transform.position, hero.ShootAt);
+                var distance = Vector3.Distance(transform.position, obj.transform.position);
 
                 if (distance > range || distance >= closestDistance) continue;
 
-                closestHero = hero;
+                closestObject = obj;
                 closestDistance = distance;
             }
 
-            return closestHero;
+            return closestObject;
         }
 
         public bool IsVisible(Hero hero)
@@ -62,13 +84,31 @@ namespace Bot
             
             return isVisible;
         }
+        
+        public bool IsVisible(Component component)
+        {
+            if (component == null) return false;
+            
+            Vector3 direction = component.transform.position - transform.position;
+            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit, _playerDetectionRadius) &&
+                             hit.collider.gameObject == component.gameObject;
+            
+            return isVisible;
+        }
 
         private void OnDrawGizmosSelected()
         {
-            Gizmos.color = Color.yellow;
+            Gizmos.color = _playerDetectionColor;
             Gizmos.DrawWireSphere(transform.position, _playerDetectionRadius);
-            Gizmos.color = Color.red;
+            
+            Gizmos.color = _playerAttackColor;
             Gizmos.DrawWireSphere(transform.position, _playerAttackRadius);
+            
+            Gizmos.color = _crateDetectionColor;
+            Gizmos.DrawWireSphere(transform.position, _crateDetectionRadius);
+            
+            Gizmos.color = _boostDetectionColor;
+            Gizmos.DrawWireSphere(transform.position, _boostDetectionRadius);
         }
     }
 }
