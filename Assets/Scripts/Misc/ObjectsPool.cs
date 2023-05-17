bc0bfe86ad644aa4c3cd697409c9cac5fc16ed6f
Tim <herew26@gmail.com>
2023-05-17 13:55:17 +0300

    feat: add crates and boosts to object pool

--- Assets/Scripts/Misc/ObjectsPool.cs
+++ Assets/Scripts/Misc/ObjectsPool.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Environment;
 using Events;
 using Player;
 using UnityEngine;
@@ -6,13 +7,16 @@ using UnityEngine.SceneManagement;
 
 namespace Misc
 {
-    public class HeroesPool : MonoSingleton<HeroesPool>
+    public class ObjectsPool : MonoSingleton<ObjectsPool>
     {
         [SerializeField] private GameEvent _onHeroesAmountChanged;
+        [SerializeField] private bool _autoFill = false;
 
         private Hero _playerPrefab;
 
         public List<Hero> Heroes { get; private set; } = new();
+        public List<Crate> Crates { get; private set; } = new();
+        public List<Boost> Boosts { get; private set; } = new();
         
         private void OnValidate()
         {
@@ -22,11 +26,39 @@ namespace Misc
         private void Start()
         {
             _onHeroesAmountChanged.Raise(this, Heroes.Count);
+            
+            if (_autoFill)
+            {
+                SetHeroes(FindObjectsOfType<Hero>());
+                SetCrates(FindObjectsOfType<Crate>());
+            }
         }
 
         public void SetPlayerPrefab(Hero playerPrefab)
         {
             _playerPrefab = playerPrefab;
+        }
+        
+        public void SetHeroes(IEnumerable<Hero> heroes)
+        {
+            Heroes.AddRange(heroes);
+            _onHeroesAmountChanged.Raise(this, Heroes.Count);
+        }
+        
+        public void SetCrates(IEnumerable<Crate> crates)
+        {
+            Crates.AddRange(crates);
+        }
+        
+        public void InstantiatePlayer()
+        {
+            Heroes.Add(Instantiate(_playerPrefab));
+            _onHeroesAmountChanged.Raise(this, Heroes.Count);
+        }
+        
+        public void AddBoost(Boost boost)
+        {
+            Boosts.Add(boost);
         }
 
         public void RemoveHero(Component sender, object data)
@@ -51,13 +83,22 @@ namespace Misc
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
             }
         }
-
-
-        public void SetHeroes(List<Hero> heroes)
+        
+        public void RemoveProp(Component sender, object data)
         {
-            Heroes.Add(Instantiate(_playerPrefab));
-
-            Heroes.AddRange(heroes);
+            if (sender.TryGetComponent(out Crate crate) && Crates.Contains(crate))
+            {
+                Crates.Remove(crate);
+                Destroy(crate.gameObject);
+                return;
+            }
+            
+            if (sender.TryGetComponent(out Boost boost) && Boosts.Contains(boost))
+            {
+                Boosts.Remove(boost);
+                Destroy(boost.gameObject);
+                return;
+            }
         }
     }
 }
