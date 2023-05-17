47a40e598328b69d50a242a0fdd58af4a7cb99f6
Tim <herew26@gmail.com>
2023-05-17 14:09:49 +0300

    ref: rename detection and attack radius

--- Assets/Scripts/Bot/BotSensor.cs
+++ Assets/Scripts/Bot/BotSensor.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Combat;
 using Environment;
 using JetBrains.Annotations;
 using Misc;
@@ -9,32 +8,29 @@ namespace Bot
 {
     public class BotSensor : MonoBehaviour
     {
-        [SerializeField] private float _playerDetectionRadius = 7f; // TODO: rename to _heroDetectionRange
+        [SerializeField] private float _heroDetectionRange = 7f;
         [SerializeField] private Color _playerDetectionColor = Color.yellow;
         
         [SerializeField] private float _crateDetectionRadius = 10f;
         [SerializeField] private Color _crateDetectionColor = Color.magenta;
-        
-        [SerializeField] private float _crateAttackRadius = 5f;
-        [SerializeField] private Color _crateAttackColor = Color.red;
-        
+
         [SerializeField] private float _boostDetectionRadius = 10f;
         [SerializeField] private Color _boostDetectionColor = Color.cyan;
         
-        [SerializeField] private float _playerAttackRadius = 5f; // TODO: rename to _attackRange
+        [SerializeField] private float _attackRange = 5f;
         [SerializeField] private Color _playerAttackColor = Color.red;
 
-        public bool IsAnyHeroInDetectionRange => IsAnyObjectInRange(_playerDetectionRadius, ObjectsPool.Instance.Heroes);
-        [CanBeNull] public Hero ClosestHeroInDetectionRange => GetClosestObjectInRange(_playerDetectionRadius, ObjectsPool.Instance.Heroes);
+        public bool IsAnyHeroInDetectionRange => IsAnyObjectInRange(_heroDetectionRange, ObjectsPool.Instance.Heroes);
+        [CanBeNull] public Hero ClosestHeroInDetectionRange => GetClosestObjectInRange(_heroDetectionRange, ObjectsPool.Instance.Heroes);
         
-        public bool IsAnyHeroInAttackRange => IsAnyObjectInRange(_playerAttackRadius, ObjectsPool.Instance.Heroes);
-        [CanBeNull] public Hero ClosestHeroInAttackRange => GetClosestObjectInRange(_playerAttackRadius, ObjectsPool.Instance.Heroes);
+        public bool IsAnyHeroInAttackRange => IsAnyObjectInRange(_attackRange, ObjectsPool.Instance.Heroes);
+        [CanBeNull] public Hero ClosestHeroInAttackRange => GetClosestObjectInRange(_attackRange, ObjectsPool.Instance.Heroes);
         
         public bool IsAnyCrateInDetectionRange => IsAnyObjectInRange(_crateDetectionRadius, ObjectsPool.Instance.Crates);
         [CanBeNull] public Crate ClosestCrateInDetectionRange => GetClosestObjectInRange(_crateDetectionRadius, ObjectsPool.Instance.Crates);
         
-        public bool IsAnyCrateInAttackRange => IsAnyObjectInRange(_crateAttackRadius, ObjectsPool.Instance.Crates);
-        [CanBeNull] public Crate ClosestCrateInAttackRange => GetClosestObjectInRange(_crateAttackRadius, ObjectsPool.Instance.Crates);
+        public bool IsAnyCrateInAttackRange => IsAnyObjectInRange(_attackRange, ObjectsPool.Instance.Crates);
+        [CanBeNull] public Crate ClosestCrateInAttackRange => GetClosestObjectInRange(_attackRange, ObjectsPool.Instance.Crates);
         
         public bool IsAnyBoostInDetectionRange => IsAnyObjectInRange(_boostDetectionRadius, ObjectsPool.Instance.Boosts);
         [CanBeNull] public Boost ClosestBoostInDetectionRange => GetClosestObjectInRange(_boostDetectionRadius, ObjectsPool.Instance.Boosts);
@@ -79,7 +75,7 @@ namespace Bot
             if (hero == null) return false;
             
             Vector3 direction = hero.ShootAt - transform.position;
-            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit, _playerDetectionRadius) &&
+            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit) &&
                              hit.collider.gameObject == hero.gameObject;
             
             return isVisible;
@@ -90,7 +86,7 @@ namespace Bot
             if (component == null) return false;
             
             Vector3 direction = component.transform.position - transform.position;
-            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit, _playerDetectionRadius) &&
+            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit) &&
                              hit.collider.gameObject == component.gameObject;
             
             return isVisible;
@@ -99,10 +95,10 @@ namespace Bot
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = _playerDetectionColor;
-            Gizmos.DrawWireSphere(transform.position, _playerDetectionRadius);
+            Gizmos.DrawWireSphere(transform.position, _heroDetectionRange);
             
             Gizmos.color = _playerAttackColor;
-            Gizmos.DrawWireSphere(transform.position, _playerAttackRadius);
+            Gizmos.DrawWireSphere(transform.position, _attackRange);
             
             Gizmos.color = _crateDetectionColor;
             Gizmos.DrawWireSphere(transform.position, _crateDetectionRadius);
