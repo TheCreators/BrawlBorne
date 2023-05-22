4819a6782af5a0fe83d6d31a59e9067616222a39
Tim <herew26@gmail.com>
2023-05-23 02:17:35 +0300

    feat: add naughty attributes, ColoredRange to bot sensor

--- Assets/Scripts/Heroes/Bot/BotMovement.cs
+++ Assets/Scripts/Heroes/Bot/BotMovement.cs
@@ -1,31 +1,40 @@
-﻿using Events;
+﻿using System.Numerics;
+using Events;
 using Misc;
+using Models;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
-namespace Bot
+namespace Heroes.Bot
 {
     [RequireComponent(typeof(NavMeshAgent))]
     public class BotMovement : MonoBehaviour
     {
-        [Header("Random Point")]
-        [SerializeField] private float _randomPointMaxRadius = 10f;
-        [SerializeField] private float _randomPointMinRadius = 5f;
-        
-        [Header("Strafe")]
-        [SerializeField] private float _strafeSpeed = 1.0f;
-        [SerializeField] private float _strafeDistance = 1.0f;
+        [SerializeField]
+        private ColoredMinMaxRange _randomPointRange;
 
-        [Header("Events")] 
-        [SerializeField] private GameEvent _onMove;
-        [SerializeField] private GameEvent _onStopMoving;
-        
+        [SerializeField] [BoxGroup(Group.Strafe)] [Label("Speed")]
+        private float _strafeSpeed = 1.0f;
+
+        [SerializeField] [BoxGroup(Group.Strafe)] [Label("Distance")]
+        private float _strafeDistance = 1.0f;
+
+        [SerializeField] [BoxGroup(Group.Events)] [Required]
+        private GameEvent _onMove;
+
+        [SerializeField] [BoxGroup(Group.Events)] [Required]
+        private GameEvent _onStopMoving;
+
         private float _strafeDirection = 1.0f;
-        private float _currentStrafe = 0.0f;
-        private bool _isStrafing = false;
+        private float _currentStrafe;
+        private bool _isStrafing;
 
         private NavMeshAgent _agent;
+
         public Vector3 Destination => _agent.destination;
 
         public float WalkSpeed
@@ -37,7 +46,6 @@ namespace Bot
                 _agent.speed = value;
             }
         }
-
 
         private void OnValidate()
         {
@@ -53,43 +61,50 @@ namespace Bot
         {
             GoToRandomDestination();
         }
-        
+
         public bool IsMoving => _agent.velocity.magnitude > 0.1f || _isStrafing;
-        
+
         public Vector2 GetNormalizedRelativeVelocity()
         {
             if (_isStrafing)
             {
                 return new Vector2(0, _strafeDirection * 0.75f);
             }
-            
+
             var velocity = transform.InverseTransformDirection(_agent.velocity);
             return new Vector2(velocity.z, -velocity.x).normalized;
         }
-        
+
+        [Button("Stop NavMeshAgent", EButtonEnableMode.Playmode)]
         public void Stop()
         {
             _agent.isStopped = true;
         }
-        
-        public void Resume()
+
+        [Button("Resume NavMeshAgent", EButtonEnableMode.Playmode)]
+        private void Resume()
         {
             _onMove.Raise(this, null);
             _agent.isStopped = false;
             _isStrafing = false;
         }
-        
+
         public void GoToDestination(Vector3 destination)
         {
+            if (destination == Destination)
+            {
+                return;
+            }
+
             Resume();
             _agent.SetDestination(destination);
         }
-        
+
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
         public void GoToRandomDestination()
         {
-            Resume();
             var randomDestination = GetRandomDestination();
-            _agent.SetDestination(randomDestination);
+            GoToDestination(randomDestination);
         }
 
         public bool HasReachedDestination()
@@ -118,24 +133,25 @@ namespace Bot
         {
             while (true)
             {
-                var randomRadius = Random.Range(_randomPointMinRadius, _randomPointMaxRadius);
+                var randomRadius = Random.Range(_randomPointRange.Min, _randomPointRange.Max);
                 var randomDirection = Random.insideUnitSphere * randomRadius;
                 randomDirection += transform.position;
                 NavMesh.SamplePosition(randomDirection, out var navMeshHit, randomRadius, -1);
                 var distance = Vector3.Distance(transform.position, navMeshHit.position);
-                if (distance >= _randomPointMinRadius && distance <= _randomPointMaxRadius)
+                if (distance >= _randomPointRange.Min && distance <= _randomPointRange.Max)
                 {
                     return navMeshHit.position;
                 }
             }
         }
-        
+
         private void OnDrawGizmosSelected()
         {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(transform.position, _randomPointMaxRadius);
-            Gizmos.DrawWireSphere(transform.position, _randomPointMinRadius);
+            Gizmos.color = _randomPointRange.MaxColor;
+            Gizmos.DrawWireSphere(transform.position, _randomPointRange.Max);
+
+            Gizmos.color = _randomPointRange.MinColor;
+            Gizmos.DrawWireSphere(transform.position, _randomPointRange.Min);
         }
     }
-
 }
---
+++ Assets/Scripts/Heroes/Bot/BotSensor.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using Environment;
+using JetBrains.Annotations;
+using Misc;
+using Models;
+using UnityEngine;
+
+namespace Heroes.Bot
+{
+    public class BotSensor : MonoBehaviour
+    {
+        [SerializeField]
+        private ColoredRange _heroDetection;
+
+        [SerializeField]
+        private ColoredRange _crateDetection;
+
+        [SerializeField]
+        private ColoredRange _boostDetection;
+
+        [SerializeField]
+        private ColoredRange _attack;
+
+        [SerializeField]
+        private LayerMask _layerMask;
+
+        public bool IsAnyHeroInDetectionRange => IsAnyObjectInRange(_heroDetection.Value, ObjectsPool.Instance.Heroes);
+
+        [CanBeNull]
+        public Hero ClosestHeroInDetectionRange => GetClosestObjectInRange(_heroDetection.Value, ObjectsPool.Instance.Heroes);
+
+        public bool IsAnyHeroInAttackRange => IsAnyObjectInRange(_attack.Value, ObjectsPool.Instance.Heroes);
+
+        [CanBeNull]
+        public Hero ClosestHeroInAttackRange => GetClosestObjectInRange(_attack.Value, ObjectsPool.Instance.Heroes);
+
+        public bool IsAnyCrateInDetectionRange => IsAnyObjectInRange(_crateDetection.Value, ObjectsPool.Instance.Crates);
+
+        [CanBeNull]
+        public Crate ClosestCrateInDetectionRange => GetClosestObjectInRange(_crateDetection.Value, ObjectsPool.Instance.Crates);
+
+        public bool IsAnyCrateInAttackRange => IsAnyObjectInRange(_attack.Value, ObjectsPool.Instance.Crates);
+
+        [CanBeNull]
+        public Crate ClosestCrateInAttackRange => GetClosestObjectInRange(_attack.Value, ObjectsPool.Instance.Crates);
+
+        public bool IsAnyBoostInDetectionRange => IsAnyObjectInRange(_boostDetection.Value, ObjectsPool.Instance.Boosts);
+
+        [CanBeNull]
+        public Boost ClosestBoostInDetectionRange => GetClosestObjectInRange(_boostDetection.Value, ObjectsPool.Instance.Boosts);
+
+        private bool IsAnyObjectInRange(float range, IEnumerable<Component> objects)
+        {
+            foreach (Component obj in objects)
+            {
+                if (obj == null || obj.gameObject.Equals(gameObject)) continue;
+
+                var distance = Vector3.Distance(transform.position, obj.transform.position);
+
+                if (distance <= range) return true;
+            }
+
+            return false;
+        }
+
+        [CanBeNull]
+        private T GetClosestObjectInRange<T>(float range, IEnumerable<T> objects) where T : Component
+        {
+            T closestObject = null;
+            var closestDistance = Mathf.Infinity;
+
+            foreach (T obj in objects)
+            {
+                if (obj == null || obj.gameObject.Equals(gameObject)) continue;
+
+                var distance = Vector3.Distance(transform.position, obj.transform.position);
+
+                if (distance > range || distance >= closestDistance) continue;
+
+                closestObject = obj;
+                closestDistance = distance;
+            }
+
+            return closestObject;
+        }
+
+        public bool IsVisible(Hero hero)
+        {
+            if (hero == null) return false;
+
+            Vector3 direction = hero.ShootAt - transform.position;
+            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit, 100000, _layerMask) &&
+                             hit.collider.gameObject == hero.gameObject;
+
+            return isVisible;
+        }
+
+        public bool IsVisible(Component component)
+        {
+            if (component == null) return false;
+
+            Vector3 direction = component.transform.position - transform.position;
+            bool isVisible = Physics.SphereCast(transform.position, 0.5f, direction, out var hit, 100000, _layerMask) &&
+                             hit.collider.gameObject == component.gameObject;
+
+            return isVisible;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = _heroDetection.Color;
+            Gizmos.DrawWireSphere(transform.position, _heroDetection.Value);
+
+            Gizmos.color = _attack.Color;
+            Gizmos.DrawWireSphere(transform.position, _attack.Value);
+
+            Gizmos.color = _crateDetection.Color;
+            Gizmos.DrawWireSphere(transform.position, _crateDetection.Value);
+
+            Gizmos.color = _boostDetection.Color;
+            Gizmos.DrawWireSphere(transform.position, _boostDetection.Value);
+        }
+    }
+}
