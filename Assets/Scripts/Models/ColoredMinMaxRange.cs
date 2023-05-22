ba9f93fe40fe69d086c3971feb026f7bd8c4a75b
Tim <herew26@gmail.com>
2023-05-23 01:54:26 +0300

    feat: add Group, ColoredRange, ColoredMinMaxRange models

---
+++ Assets/Scripts/Models/ColoredMinMaxRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public struct ColoredMinMaxRange
+    {
+        [SerializeField] [MinMaxSlider(0, 200)]
+        private Vector2 _range;
+
+        [SerializeField]
+        private Color _minColor;
+
+        [SerializeField]
+        private Color _maxColor;
+
+        public float Min => _range.x;
+        public float Max => _range.y;
+        public Color MinColor => _minColor;
+        public Color MaxColor => _maxColor;
+    }
+}
---
+++ Assets/Scripts/Models/ColoredRange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public struct ColoredRange
+    {
+        [SerializeField] [Min(0)]
+        private float _range;
+
+        [SerializeField]
+        private Color _color;
+
+        public float Value => _range;
+        public Color Color => _color;
+    }
+}
