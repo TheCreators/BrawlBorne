4efb88ce6f618ffbc6bfe07863876136d95487ae
Tim <herew26@gmail.com>
2023-05-23 02:14:11 +0300

    feat: add naughty attributes, current and max cells properties to weapon

--- Assets/Scripts/Combat/Weapons/Weapon.cs
+++ Assets/Scripts/Combat/Weapons/Weapon.cs
@@ -1,30 +1,86 @@
-using System;
 using System.Collections;
 using Events;
 using Misc;
+using Models;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Combat.Weapons
 {
     public abstract class Weapon : MonoBehaviour
     {
-        [Header("Cells")] 
-        [SerializeField, Min(1)] private int _maxCells = 3;
-        [SerializeField, Min(0)] private int _currentCells = 3;
-        [SerializeField, Min(0)] private float _refillTime = 2f;
-        
-        [Header("Events")]
-        [SerializeField] protected GameEvent _onUse;
-        [SerializeField] protected GameEvent _onCellsAmountChanged;
-        
-        [Header("Damage")]
-        [SerializeField, Min(0)] protected float _damage = 5f;
-        
-        [Header("Hit Layers")]
-        [SerializeField] protected LayerMask _hitLayers;
+        [SerializeField] [BoxGroup(Group.Cells)] [ProgressBar("Cells", nameof(_maxCells), EColor.Orange)]
+        private int _cellsBar = 3;
+
+        [SerializeField] [BoxGroup(Group.Cells)] [Min(0)]
+        [OnValueChanged(nameof(OnMaxCellsChangedUsingInspector))]
+        private int _maxCells = 3;
+
+        [SerializeField] [BoxGroup(Group.Cells)] [Min(0)]
+        [OnValueChanged(nameof(OnCurrentCellsChangedUsingInspector))]
+        private int _currentCells = 3;
+
+        [SerializeField] [BoxGroup(Group.Cells)] [Min(0)]
+        private float _refillTime = 2f;
+
+        [SerializeField] [BoxGroup(Group.Events)] [Required]
+        protected GameEvent _onUse;
+
+        [SerializeField] [BoxGroup(Group.Events)] [Required]
+        protected GameEvent _onCellsAmountChanged;
+
+        [SerializeField] [Min(0)] [BoxGroup(Group.Hit)]
+        protected float _damage = 5f;
+
+        [SerializeField] [BoxGroup(Group.Hit)]
+        protected LayerMask _hitLayers;
 
         protected bool CanBeUsed = true;
-        private bool _isRefilling = false;
+        private bool _isRefilling;
+
+        private int CurrentCells
+        {
+            get => _currentCells;
+            set
+            {
+                if (value < 0)
+                {
+                    _currentCells = 0;
+                }
+                else if (value > _maxCells)
+                {
+                    _currentCells = _maxCells;
+                }
+                else
+                {
+                    _currentCells = value;
+                }
+
+                _cellsBar = CurrentCells;
+                _onCellsAmountChanged.Raise(this, CurrentCells);
+            }
+        }
+
+        private int MaxCells
+        {
+            get => _maxCells;
+            set
+            {
+                if (value < 0)
+                {
+                    _maxCells = 0;
+                }
+                else
+                {
+                    _maxCells = value;
+                }
+
+                if (CurrentCells > MaxCells)
+                {
+                    CurrentCells = MaxCells;
+                }
+            }
+        }
 
         protected virtual void OnValidate()
         {
@@ -34,46 +90,51 @@ namespace Combat.Weapons
 
         protected virtual void Start()
         {
-            _onCellsAmountChanged.Raise(this, _currentCells);
+            CurrentCells = _currentCells;
+            MaxCells = _maxCells;
         }
 
         protected abstract void Use();
 
         public void TryUse()
         {
-            if (CanBeUsed is false || _currentCells <= 0)
+            if (CanBeUsed is false || CurrentCells == 0)
             {
                 return;
             }
 
-            _currentCells--;
-            _onCellsAmountChanged.Raise(this, _currentCells);
+            CurrentCells--;
+            _onCellsAmountChanged.Raise(this, CurrentCells);
             if (_isRefilling is false)
             {
                 StartCoroutine(Refill());
             }
-            
+
             _onUse.Raise(this, null);
             Use();
-        }
-        
-        private IEnumerator Refill()
-        {
-            _isRefilling = true;
-
-            while (_currentCells < _maxCells)
-            {
-                yield return new WaitForSeconds(_refillTime);
-                _currentCells++;
-                _onCellsAmountChanged.Raise(this, _currentCells);
-            }
-
-            _isRefilling = false;
         }
 
         public void IncreaseDamage(float increasePercent)
         {
             _damage *= 1 + increasePercent / 100;
         }
+
+        private IEnumerator Refill()
+        {
+            _isRefilling = true;
+
+            while (CurrentCells != MaxCells)
+            {
+                yield return new WaitForSeconds(_refillTime);
+                CurrentCells++;
+                _onCellsAmountChanged.Raise(this, CurrentCells);
+            }
+
+            _isRefilling = false;
+        }
+
+        private void OnCurrentCellsChangedUsingInspector() => CurrentCells = _currentCells;
+
+        private void OnMaxCellsChangedUsingInspector() => MaxCells = _maxCells;
     }
 }
