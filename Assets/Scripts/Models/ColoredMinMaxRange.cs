b548e9ca666f0602b762dddda474450b4d70352b
Tim <herew26@gmail.com>
2023-06-02 13:23:01 +0300

    feat: add models

--- Assets/Scripts/Models/ColoredMinMaxRange.cs
+++ Assets/Scripts/Models/ColoredMinMaxRange.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using NaughtyAttributes;
 using UnityEngine;
 
 namespace Models
 {
     [Serializable]
-    public struct ColoredMinMaxRange
+    public class ColoredMinMaxRange : MinMaxRange
     {
-        [SerializeField] [MinMaxSlider(0, 200)]
-        private Vector2 _range;
+        [SerializeField]
+        private Color _minColor = Color.black;
 
         [SerializeField]
-        private Color _minColor;
-
-        [SerializeField]
-        private Color _maxColor;
-
-        public float Min => _range.x;
-        public float Max => _range.y;
+        private Color _maxColor = Color.black;
+        
         public Color MinColor => _minColor;
         public Color MaxColor => _maxColor;
     }
